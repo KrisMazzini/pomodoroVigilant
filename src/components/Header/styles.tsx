493ced2a36503ee87aabcadef02e0b571d337010
Kristopher Mazzini <kristophermazzini.sc@gmail.com>
2022-09-21 21:01:16 -0300

    adjust theme colors

--- src/components/Header/styles.tsx
+++ src/components/Header/styles.tsx
@@ -7,27 +7,29 @@ export const Container = styled.div`
     justify-content: center;
     align-items: center;
     gap: 10px;
-
+    
     img {
         width: 100px;
     }
-
+    
     div {
         display: flex;
         flex-direction: column;
         align-items : center;
         gap: 5px;
     }
-
+    
     h1 {
         font-family: var(--syncopate);
         font-size: var(--size-lg);
         font-weight: 900;
+        color: var(--light);
     }
-
+    
     h2 {
         font-family: var(--inter);
         font-size: var(--size-md);
         font-weight: 400;
+        color: var(--light);
     }
 `
